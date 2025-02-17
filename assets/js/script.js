d7ac248d42b65199ae67df4b7ad8a35454fa83f8
souta <kansouta1125@icloud.com>
2025-02-17 21:09:47 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// elements
+const tableEvents = document.getElementById("tableEvents");
+const loading = document.getElementById("loading");
+
+// Web API
+const apiUrl =
+"https://script.google.com/macros/s/AKfycbxvQ4ZH6O3s6EebcZPuXL9_pnRYbCE3DzaGKXURhTrhUmWVdTZYu0_dj9CmbeyjAzIEiA/exec";
+
+// get data
+fetch(`${apiUrl}?request=events`)
+.then((res) => {
+  return res.json();
+})
+.then((data) => {
+  data.body.data.forEach((e) => {
+      const row = tableEvents.querySelector(".skeleton .row").cloneNode(true);
+      const time = e.updateTime;
+      const type = e.eventType;
+      const userName = e.displayName;
+      const msg = e.message;
+
+      row.querySelector(".time").textContent = time;
+      row.querySelector(".type").textContent = type;
+      row.setAttribute("data-type", type);
+      row.querySelector(".userName").textContent = userName;
+      row.querySelector(".msg").textContent = msg;
+
+      tableEvents.appendChild(row);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    loading.classList.add("is-hide");
+  });
+
+// filter
+const filter = document.getElementById("filter");
+filter.addEventListener("change", (e) => {
+  const type = e.target.value;
+  console.log(type);
+  const rows = tableEvents.querySelectorAll(".row");
+  rows.forEach((row) => {
+    if (type === "all" || row.getAttribute("data-type") === type || row.classList.contains("header")) {
+      row.classList.remove("is-hide");
+    } else {
+      row.classList.add("is-hide");
+    }
+  });
+})
